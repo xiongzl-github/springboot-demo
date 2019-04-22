3c5ba6af687e8213b5f8a98cb40afbc2b00dfdbf
xiongzl-github <xiongzl666@163.com>
2019-04-22 18:53:28 +0800

    使用sringboot搭建在线教育网站

---
+++ xd-online-education-project/src/main/java/top/timebook/xdonlineeducationproject/XdOnlineEducationProjectApplication.java
@@ -0,0 +1,14 @@
+package top.timebook.xdonlineeducationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdOnlineEducationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdOnlineEducationProjectApplication.class, args);
+		System.out.println("hello world!");
+	}
+
+}
